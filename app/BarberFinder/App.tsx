f109a546921fa9c6935920df5888a47b28e88cd1
Gijs van Lokven <gijs.vanlokven@edu-kw1c.nl>
2019-11-04 14:10:45 +0100

    Initial Commit

---
+++ app/BarberFinder/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textContainer}>Test Text</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContainer: {
+    position: "absolute",
+    right: 0,
+    top: 50
+  }
+});
